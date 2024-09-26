96a2201b58cecd04b1827da28067a248ca031bb7
AndreasIsl <92983953+AndreasIsl@users.noreply.github.com>
2024-09-26 15:50:19 +0200

    added netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,11 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: 'server',
+  adapter: netlify()
 });
